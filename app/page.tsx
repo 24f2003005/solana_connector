9848ad8f4cbaa604e134dee6b23423f541ad6814
24f2003005 <23f3000982@ds.study.iitm.ac.in>
2026-01-26 13:00:59 +0530

    v0.1.5

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <span className={styles.version}>Version: 0.1.3</span>
+      <span className={styles.version}>Version: 0.1.5</span>
 
       <div className={styles.eachButton}>
         <span>Light Theme,SolBalance </span>
